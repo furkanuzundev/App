99d8825113ec2a00556096b5381bc221c37c8d35
Furkan <furkanuzundev@gmail.com>
2022-10-27 10:53:20 +0300

    Update anchorForAttachmentsOnlyPropTypes.js

--- src/components/AnchorForAttachmentsOnly/anchorForAttachmentsOnlyPropTypes.js
+++ src/components/AnchorForAttachmentsOnly/anchorForAttachmentsOnlyPropTypes.js
@@ -2,20 +2,21 @@ import PropTypes from 'prop-types';
 import stylePropTypes from '../../styles/stylePropTypes';
 
 const propTypes = {
-    /** The URL of the attachment */
-    source: PropTypes.string,
+  /** The URL of the attachment */
+  source: PropTypes.string,
 
-    /** Filename for attachments. */
-    displayName: PropTypes.string,
+  /** Filename for attachments. */
+  displayName: PropTypes.string,
 
-    /** Any additional styles to apply */
-    style: stylePropTypes,
+  /** Any additional styles to apply */
+  style: stylePropTypes,
 };
 
 const defaultProps = {
-    source: '',
-    style: {},
-    displayName: '',
+  source: '',
+  style: {},
+  displayName: '',
+  test: '123',
 };
 
 export {propTypes, defaultProps};
