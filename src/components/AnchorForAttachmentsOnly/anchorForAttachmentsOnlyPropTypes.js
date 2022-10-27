9558673aeba44660c8574c468df83fb6d378298f
Furkan <furkanuzundev@gmail.com>
2022-10-27 10:57:21 +0300

    Revert "Update anchorForAttachmentsOnlyPropTypes.js"
    This reverts commit 99d8825113ec2a00556096b5381bc221c37c8d35.

--- src/components/AnchorForAttachmentsOnly/anchorForAttachmentsOnlyPropTypes.js
+++ src/components/AnchorForAttachmentsOnly/anchorForAttachmentsOnlyPropTypes.js
@@ -2,21 +2,20 @@ import PropTypes from 'prop-types';
 import stylePropTypes from '../../styles/stylePropTypes';
 
 const propTypes = {
-  /** The URL of the attachment */
-  source: PropTypes.string,
+    /** The URL of the attachment */
+    source: PropTypes.string,
 
-  /** Filename for attachments. */
-  displayName: PropTypes.string,
+    /** Filename for attachments. */
+    displayName: PropTypes.string,
 
-  /** Any additional styles to apply */
-  style: stylePropTypes,
+    /** Any additional styles to apply */
+    style: stylePropTypes,
 };
 
 const defaultProps = {
-  source: '',
-  style: {},
-  displayName: '',
-  test: '123',
+    source: '',
+    style: {},
+    displayName: '',
 };
 
 export {propTypes, defaultProps};
